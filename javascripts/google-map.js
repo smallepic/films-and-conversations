5e73cbea0f9ffbd371730fb66865942f6ebe8a9e
John Ngo <john@smallepic.com>
2015-01-12 17:47:14 +0100

    Finalised design for home page.

---
+++ javascripts/google-map.js
@@ -0,0 +1,24 @@
+var container = document.getElementById('venue-google-map');
+var address = container.getAttribute('data-address');
+var info_window = container.getAttribute('data-info-window');
+var geocoder = new google.maps.Geocoder();
+var options =
+{
+  disableDefaultUI: true,
+  disableDoubleClickZoom: true,
+  draggable: false,
+  mapTypeId: google.maps.MapTypeId.ROADMAP,
+  scrollwheel: false,
+  zoom: 15
+};
+
+geocoder.geocode({address: address}, function(results, status)
+{
+  if (status == google.maps.GeocoderStatus.OK)
+  {
+    var map = new google.maps.Map(container, options);
+    var marker = new google.maps.Marker({map: map, position: results[0].geometry.location, title: address});
+
+    map.setCenter(new google.maps.LatLng(marker.getPosition().lat(), marker.getPosition().lng()));
+  }
+});
